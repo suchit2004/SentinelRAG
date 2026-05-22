d23c53c1a283d866b6d702a70bbca99562eef35e
Suchit Jundare <suchitjundare03@gmail.com>
2026-05-22 22:02:53 +0530

    Updated ingestion.py

--- app/ingestion/ingestion.py
+++ app/ingestion/ingestion.py
@@ -5,3 +5,23 @@ from qdrant_client import QdrantClient
 from qdrant_client.models import Distance, VectorParams
 from dotenv import load_dotenv
 import os
+
+load_dotenv()
+embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
+
+client = QdrantClient(path="vectorstore")
+
+client.recreate_collection(
+    collection_name="company_docs",
+    vectors_config=VectorParams(size=384, distance=Distance.COSINE)
+)
+
+loader = PyPDFLoader("data/finance_report.pdf")
+documents = loader.load()
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=500,
+    chunk_overlap=50
+)
+
+chunks = splitter.split_documents(documents)
